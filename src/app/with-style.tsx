bcc68d0166130034a876f9cc6bfbdd9a59d086e5
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-10-04 11:51:59 +0900

    feat: 전역 min-height 설정

--- src/app/with-style.tsx
+++ src/app/with-style.tsx
@@ -1,7 +1,11 @@
 import { ReactNode } from "react";
 
 function StyleProvider({ children }: { children: ReactNode }) {
-  return <div className="bg-butter font-square-round p-10">{children}</div>;
+  return (
+    <div className="min-h-screen px-8 py-14 bg-butter font-square-round">
+      {children}
+    </div>
+  );
 }
 
 export default StyleProvider;
